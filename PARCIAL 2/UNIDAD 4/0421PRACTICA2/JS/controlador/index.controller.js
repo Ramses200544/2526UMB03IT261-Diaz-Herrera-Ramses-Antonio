3cecf6a12d64457f485d7b01fdd8feb396243e20
Ramses200544 <ramsesantoniodiazherrera490@gmail.com>
2026-04-27 15:57:53 -0600

    Angular JS

---
+++ PARCIAL 2/UNIDAD 4/0421PRACTICA2/JS/controlador/index.controller.js
@@ -0,0 +1,16 @@
+angular.module('app').controler('Index', Index);
+index.$inject = ['$scope', '$log'];
+function Index($scope, $log) {
+    console.log('Hola');
+    alert("SoyunMensaje")
+    
+}
+
+
+angular.module('app').controler('Index', Index);
+index.$inject = ['$scope', '$log'];
+function Hola($scope, $log) {
+    alert("Hola mundo");
+}
+
+
