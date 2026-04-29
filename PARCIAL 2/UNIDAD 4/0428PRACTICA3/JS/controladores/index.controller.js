fc79a1ef38ff6d3ed01608d454813ede40a0b4fb
Ramses200544 <ramsesantoniodiazherrera490@gmail.com>
2026-04-29 14:19:30 -0600

    Commit

---
+++ PARCIAL 2/UNIDAD 4/0428PRACTICA3/JS/controladores/index.controller.js
@@ -0,0 +1,12 @@
+angular.module('app', []).controller('Rutascontroller', Index);
+Index.$inject($scope);
+
+function Index($scope) {
+
+    $scope.proveedores = ['Proveedor A', 'Proveedor B']
+
+  
+}
+
+
+
