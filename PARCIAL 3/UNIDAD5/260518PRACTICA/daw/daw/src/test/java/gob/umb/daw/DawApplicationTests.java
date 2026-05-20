edba07e5c015143c5161626d75e7585aea0f4b11
Ramses200544 <ramsesantoniodiazherrera490@gmail.com>
2026-05-20 14:42:33 -0600

    Hola

---
+++ PARCIAL 3/UNIDAD5/260518PRACTICA/daw/daw/src/test/java/gob/umb/daw/DawApplicationTests.java
@@ -0,0 +1,13 @@
+package gob.umb.daw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
